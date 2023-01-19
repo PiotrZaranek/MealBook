f9d4a926d57b9cded7cfe331431e6d6b156cd77b
Piotr Zaranek <piotrek.zaranek@gmail.com>
2023-01-19 12:42:51 +0100

    Added Context configuration.

--- MealBook.Infrastructure/Context.cs
+++ MealBook.Infrastructure/Context.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,15 @@ using System.Threading.Tasks;
 
 namespace MealBook.Infrastructure
 {
-    public class Context
+    public class Context : IdentityDbContext
     {
+        public Context(DbContextOptions options) : base(options)
+        {
+        }
 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
     }
 }
