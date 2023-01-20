a1152684eed7ae3ea3add08ddfed7235533ff9ad
PiotrZaranek <piotrek.zaranek@gmail.com>
2023-01-20 13:44:36 +0100

    Added Tag model.

---
+++ MealBook.Domain/Model/Tag.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MealBook.Domain.Model
+{
+    public class Tag
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        // n:m To MealTag
+        public ICollection<MealTag> MealTag { get; set; }
+    }
+}
