1b2125e755314188a4f932a621738a583047327f
PiotrZaranek <piotrek.zaranek@gmail.com>
2023-01-20 13:53:08 +0100

    Nothing changed.

--- MealBook.Infrastructure/Context.cs
+++ MealBook.Infrastructure/Context.cs
@@ -1,15 +1,19 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using MealBook.Domain.Model;
+using System.Reflection.Metadata.Ecma335;
 
 namespace MealBook.Infrastructure
 {
     public class Context : IdentityDbContext
     {
+        public DbSet<Ingredient> Ingredients { get; set; }
+        public DbSet<Meal> Meals { get; set; }
+        public DbSet<MealDetalInformation> MealDetalInformations { get; set; }
+        public DbSet<MealIngredniet> MealIngredniet { get; set; }
+        public DbSet<MealTag> MealTag { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
         public Context(DbContextOptions options) : base(options)
         {
         }
@@ -17,6 +21,39 @@ namespace MealBook.Infrastructure
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+
+            // 1:1 Meal - MealDetalInformation
+            builder.Entity<MealDetalInformation>()
+                .HasOne(a => a.Meal).WithOne(b => b.MealDetalInformation)
+                .HasForeignKey<Meal>(e => e.MealDetalInformationsId);
+
+            // n:m Meal - Tag
+            builder.Entity<MealTag>()
+                .HasKey(it => new { it.MealId, it.TagId });
+
+            builder.Entity<MealTag>()
+                .HasOne<Meal>(it => it.Meal)
+                .WithMany(i => i.MealTag)
+                .HasForeignKey(k => k.MealId);
+
+            builder.Entity<MealTag>()
+                .HasOne<Tag>(it => it.Tag)
+                .WithMany(i => i.MealTag)
+                .HasForeignKey(k => k.TagId);
+
+            // n:m Meal - Ingredient
+            builder.Entity<MealIngredniet>()
+                .HasKey(k => new { k.MealId, k.IngredientId });
+
+            builder.Entity<MealIngredniet>()
+                .HasOne<Meal>(it => it.Meal)
+                .WithMany(i => i.MealIngredniet)
+                .HasForeignKey(k => k.MealId);
+
+            builder.Entity<MealIngredniet>()
+                .HasOne<Ingredient>(it => it.Ingredient)
+                .WithMany(i => i.MealIngredniet)
+                .HasForeignKey(k => k.IngredientId);
         }
     }
 }
