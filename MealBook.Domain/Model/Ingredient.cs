79c3006cb575160c8abd51fc990858a79ce08f82
PiotrZaranek <piotrek.zaranek@gmail.com>
2023-01-20 13:42:56 +0100

    Added model Ingredient.

---
+++ MealBook.Domain/Model/Ingredient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MealBook.Domain.Model
+{
+    public class Ingredient
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Value { get; set; }
+        public string Unit { get; set; }
+
+        // n:m To MealIngredient
+        public ICollection<MealIngredniet> MealIngredniet { get; set; }
+    }
+}
