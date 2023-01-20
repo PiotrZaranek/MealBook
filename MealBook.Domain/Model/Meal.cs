47a54af978a48663895de01c900ddb6089506b5d
PiotrZaranek <piotrek.zaranek@gmail.com>
2023-01-20 13:43:30 +0100

    Added Meal model.

---
+++ MealBook.Domain/Model/Meal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MealBook.Domain.Model
+{
+    public class Meal
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }        
+        public string Desctiption { get; set; }
+        public DateTime Date { get; set; }
+        public int Likes { get; set; }
+
+        // 1:1 To MealDetalInformation
+        public int MealDetalInformationsId { get; set; }
+        public MealDetalInformation MealDetalInformation { get; set; }
+
+        // 1:1 To User
+        public int UserId { get; set; }
+
+        // n:m To MealTag
+        public ICollection<MealTag> MealTag { get; set; }
+
+        // n:m To MealIngredient
+        public ICollection<MealIngredniet> MealIngredniet { get; set; }
+    }
+}
