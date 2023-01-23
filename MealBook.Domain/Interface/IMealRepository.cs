25840228e40ea6f1601db61a4b7cb69d0f2e26df
Piotr Zaranek <piotrek.zaranek@gmail.com>
2023-01-23 16:06:12 +0100

    Added interface IMealRepository.

---
+++ MealBook.Domain/Interface/IMealRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MealBook.Domain.Model;
+using Microsoft.VisualBasic.FileIO;
+
+namespace MealBook.Domain.Interface
+{
+    public interface IMealRepository
+    {
+        int AddMeal(Meal meal);
+        void RemoveMeal(int id);
+        void UpdateMeal(Meal meal);
+        IQueryable<Meal> ListMeals();
+        Meal GetMealById(int id);
+    }
+}
